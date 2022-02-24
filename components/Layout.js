677f7d9cfc682e79d53386ea03a43cee4106450f
rhtm123 <maurya.iitk@gmail.com>
2022-02-24 13:16:11 +0530

    v0.0.0

--- components/Layout.js
+++ components/Layout.js
@@ -3,9 +3,12 @@ import Header from "./Header"
 
 import Footer from "./Footer"
 
+import Script from "next/script"
+
 
 const Layout = ({children}) => {
   return (
+    <body>
     <div className="main-wrapper">
         <Header />
 
@@ -25,6 +28,26 @@ const Layout = ({children}) => {
 
         <Footer />
     </div>
+
+
+    {/* <!-- Modernizer & jQuery JS --> */}
+    <Script src="/js/vendor/modernizr-3.11.2.min.js" />
+    <Script src="/js/vendor/jquery-3.5.1.min.js" />
+
+    {/* <!-- Bootstrap JS --> */}
+    <Script src="/js/plugins/popper.min.js" />
+    <Script src="/js/plugins/bootstrap.min.js" />
+
+
+
+    {/* <!--====== Use the minified version files listed below for better performance and remove the files listed above ======--> */}
+    <Script src="/js/plugins.min.js" />
+
+
+    {/* <!-- Main JS --> */}
+    <Script strategy="lazyOnload" src="/js/main.js" />
+
+    </body>
   )
 }
 
